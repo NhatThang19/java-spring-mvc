f6abca70f9bc1227c1bc9b033c9a09c802d3d9d7
NhatThang19 <lanhatthang1910@gmail.com>
2024-06-30 05:17:07 +0700

    119

--- src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/dto/RegisterDTO.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.domain.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import vn.hoidanit.laptopshop.service.validator.RegisterChecked;
 
 @RegisterChecked
@@ -10,6 +12,7 @@ public class RegisterDTO {
     private String password;
     private String confirmPassword;
 
+    @Size(min = 3, message = "FirstName phải có tối thiểu 3 ký tự !!!")
     public String getFirstName() {
         return firstName;
     }
@@ -26,6 +29,7 @@ public class RegisterDTO {
         this.lastName = lastName;
     }
 
+    @Email(message = "Định dạng Email không hợp lệ", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     public String getEmail() {
         return email;
     }
@@ -42,6 +46,7 @@ public class RegisterDTO {
         this.password = password;
     }
 
+    @Size(min = 3, message = "ConfirmPassword phải có tối thiểu 3 ký tự !!!")
     public String getConfirmPassword() {
         return confirmPassword;
     }

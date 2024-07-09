d18443067587035adcd48c16e420c471d49a270f
NhatThang19 <lanhatthang1910@gmail.com>
2024-07-09 13:58:56 +0700

    upload

--- src/main/java/vn/hoidanit/laptopshop/service/OrderService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/OrderService.java
@@ -3,10 +3,13 @@ package vn.hoidanit.laptopshop.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.Order;
 import vn.hoidanit.laptopshop.domain.OrderDetail;
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.repository.OrderDetailRepository;
 import vn.hoidanit.laptopshop.repository.OrderRepository;
 
@@ -22,8 +25,8 @@ public class OrderService {
         this.orderRepository = orderRepository;
     }
 
-    public List<Order> fetchAllOrders() {
-        return this.orderRepository.findAll();
+    public Page<Order> fetchAllOrders(Pageable page) {
+        return this.orderRepository.findAll(page);
     }
 
     public Optional<Order> fetchOrderById(long id) {
@@ -52,4 +55,9 @@ public class OrderService {
             this.orderRepository.save(currentOrder);
         }
     }
+
+    public List<Order> fetchOrderByUser(User user) {
+        return this.orderRepository.findByUser(user);
+    }
+
 }
